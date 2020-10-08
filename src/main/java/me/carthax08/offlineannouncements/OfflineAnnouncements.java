c48e35095452f67e2b3af8bf6feda97e93bb2cc2
CarThax08 <carson.thackston@gmail.com>
2020-10-08 09:19:46 -0400

    BETA 2.0

--- src/main/java/me/carthax08/offlineannouncements/OfflineAnnouncements.java
+++ src/main/java/me/carthax08/offlineannouncements/OfflineAnnouncements.java
@@ -8,10 +8,14 @@ import me.carthax08.offlineannouncements.events.onPlayerJoin;
 
 public final class OfflineAnnouncements extends JavaPlugin {
 
+    String version = "BETA 2.0-ALPHA";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getConsoleSender().sendMessage(ChatColor.RESET + "[OfflineAnnouncements] Loading Plugin, Please wait...");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[OfflineAnnouncements] Plugin version " + version + "is present.");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[OfflineAnnouncements] Please check the GitHub for a newer version.");
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[OfflineAnnouncements] The plugin is loaded, enjoy!");
         getCommand("offlineannouncements").setExecutor(new MainCommand(this));
         getServer().getPluginManager().registerEvents(new onPlayerJoin(this), this);
